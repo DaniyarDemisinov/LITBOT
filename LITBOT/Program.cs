bf566131295ee4e2485c3424ab24c1817ee3ad60
DaniyarDemisinov <demisinov.daniyar1997@gmail.com>
2024-12-19 21:38:02 +0500

    Удалил всё. Пишу по новой!

--- LITBOT/Program.cs
+++ LITBOT/Program.cs
@@ -4,8 +4,7 @@
     {
         static async Task Main(string[] args)
         {
-            TelegramBotUI telegram = new TelegramBotUI();
-            telegram.Start();
+
             Console.ReadLine();
         }
     }
